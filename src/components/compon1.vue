645c363bbab3bfddf59e4459a0a0a0470c2b36e7
胡三丰 <husanfeng@dev.com>
2018-11-12 16:16:12 +0800

    背景图片更换

--- src/components/compon1.vue
+++ src/components/compon1.vue
@@ -5,7 +5,7 @@
             <div class="cont" v-if="show">
                 <!-- <img class="avatar" src="https://github.com/husanfeng/person-resume/blob/master/static/img/hsf.JPG?raw=true" alt=""> -->
                 <img class="avatar" src="../../static/img//hsf.JPG" alt="">
-                <h2>优秀的判断来自经验,但经验来自错误的判断</h2>
+                <h2>不求与人相比,但求超越自己</h2>
                 <h4>我叫胡三丰</h4>
                 <h4>3年移动端和前端经验</h4>
                 <div class="state">
